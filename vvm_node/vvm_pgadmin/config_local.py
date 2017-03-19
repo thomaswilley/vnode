8e12937b7e915360372bf02f037d3550780c8d71
Thomas Willey <tomwilleyonline@hotmail.com>
2017-03-19 18:57:30 -0400

    added pgAdmin4 service with very basic integration to the node

---
+++ vvm_node/vvm_pgadmin/config_local.py
@@ -0,0 +1,8 @@
+SERVER_MODE = False
+DEFAULT_SERVER = '0.0.0.0'
+DEFAULT_SERVER_PORT = 5051
+
+LOG_FILE = '/var/log/pgadmin4/pgadmin4.log'
+SQLITE_PATH = '/var/lib/pgadmin4/pgadmin4.db'
+SESSION_DB_PATH = '/var/lib/pgadmin4/sessions'
+STORAGE_DIR = '/var/lib/pgadmin4/storage'
